f901146aa2ee13f0e96e8d6776f95b6620b66a17
librarymogilevregion <librarymogilevregion>
2026-03-16 16:47:36 +0300

    Not all changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,15 @@ import { AppRouter } from "./components/AppRouter";
 import { Navbar } from "./components/Navbar";
 import { Layout } from "antd";
 import './App.css';
+import { useEffect } from "react";
 
 export const App = () => {
+
+  useEffect(() => {
+    if(localStorage.getItem('auth')) {
+
+    }
+  }, [])
 
   return (
     <Provider store={store}>
